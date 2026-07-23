d1a3fd84bca5d694b4d30278a2442ab729dcfd48
muhammeddygt <96614965+muhammeddygt@users.noreply.github.com>
2026-07-23 10:19:56 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = 'birikim-defteri-v5';
+const APP_SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(APP_SHELL)).catch(() => {}));
+  self.skipWaiting();
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then((res) => {
+      const copy = res.clone();
+      caches.open(CACHE).then((cache) => cache.put(e.request, copy));
+      return res;
+    }).catch(() => caches.match(e.request).then((r) => r || caches.match('./index.html')))
+  );
+});
